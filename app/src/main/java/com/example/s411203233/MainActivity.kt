c70888c04475f9cc00e22c2a3c59a9e1c2a94912
jin10 <st1034322@gmail.com>
2024-12-09 15:38:25 +0800

    v2_陳錦璇

--- app/src/main/java/com/example/s411203233/MainActivity.kt
+++ app/src/main/java/com/example/s411203233/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.s411203233
 
+
+import android.app.Activity
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.s411203233.ui.theme.S411203233Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +35,12 @@ class MainActivity : ComponentActivity() {
             S411203233Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+
+
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -30,9 +49,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff95fe95))
     )
+    Column( horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(
+            "2024期末上機考(資管二B陳錦璇)",
+            modifier = modifier
+        )
+        Image(
+            painter = painterResource(id = R.drawable.class_b),
+            contentDescription = "B班"
+        )
+        Text(
+            "遊戲持續時間 0 秒",
+            modifier = modifier
+        )
+        Text(
+            "您的成績 0 分",
+            modifier = modifier
+        )
+        val activity = (LocalContext.current as? Activity)
+        Button(
+            onClick = {
+                activity?.finish()
+            }
+        ) {
+            Text("結束App")
+
+        }
+
+
+
+    }
 }
 
